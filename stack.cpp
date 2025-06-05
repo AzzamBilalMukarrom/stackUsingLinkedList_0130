0e281e08a08240ed4838d2ed281ae2dab2273fa4
AzzamBilalMukarrom <bilaldenker020@gmail.com>
2025-06-05 14:56:30 +0700

    menambahkan fungsi push pada class stack

--- stack.cpp
+++ stack.cpp
@@ -27,4 +27,16 @@ class Stack
     {
         top = NULL;
     }
+
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
+    }
+
+
 };
