1c06ecf2a168fa876a0dc9db7a24449ca718bef8
AzzamBilalMukarrom <bilaldenker020@gmail.com>
2025-06-05 15:33:46 +0700

    Implement stack content display using linked list

--- stack.cpp
+++ stack.cpp
@@ -61,5 +61,19 @@ class Stack
         return;
         }
         
-   
-}
+    else
+    {
+        Node *current = top;
+        while ( current != NULL)
+        {
+            cout << current->data << "" << endl;
+            current = current->next;
+        }
+        cout <<endl;
+    }
+    }
+
+};
+
+
+
